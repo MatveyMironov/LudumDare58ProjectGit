08f091fed6607e4cfaea7f00fad82450630527ff
MatveyMironov <matveymironof@gmail.com>
2025-10-05 22:44:19 +0300

    animation

--- LudumDare58Project/Assets/_Source/Movement/MovementControllerMB.cs
+++ LudumDare58Project/Assets/_Source/Movement/MovementControllerMB.cs
@@ -6,12 +6,14 @@ public class MovementControllerMB : MonoBehaviour
     [SerializeField] private float speed;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     private Vector2 _velocity;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -21,8 +23,15 @@ public class MovementControllerMB : MonoBehaviour
 
     public void Move(Vector2 direction)
     {
+        direction.y = 0.0f;
         direction.Normalize();
         _velocity = direction * speed;
+
+        if (_velocity.x != 0.0f)
+            _animator.SetBool("IsMoving", true);
+        else
+            _animator.SetBool("IsMoving", false);
+
         if (direction.x > 0)
             transform.localScale = new(-1.0f, 1.0f, 1.0f);
         else if (direction.x < 0)
