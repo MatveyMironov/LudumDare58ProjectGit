0fe3b3e1c03a26c41c2557266964173cb6a20058
MatveyMironov <matveymironof@gmail.com>
2025-10-05 15:07:40 +0300

    game start controller

---
+++ LudumDare58Project/Assets/_Source/Game Start Controller/GameStartControllerMB.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStartControllerMB : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnGameStart;
+
+    private void Start()
+    {
+        OnGameStart.Invoke();
+    }
+}
