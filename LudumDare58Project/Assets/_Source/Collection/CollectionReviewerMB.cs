6a65bc577d0ae91f0f66c00a0ee5171c04b885cb
Siosti <158834325+Siosti@users.noreply.github.com>
2025-10-07 00:24:16 +0300

    commit 2

--- LudumDare58Project/Assets/_Source/Collection/CollectionReviewerMB.cs
+++ LudumDare58Project/Assets/_Source/Collection/CollectionReviewerMB.cs
@@ -6,12 +6,15 @@ public class CollectionReviewerMB : MonoBehaviour
     [SerializeField] private CollectableItemSO[] requiredItems = new CollectableItemSO[0];
     [Space]
     [SerializeField] private UnityEvent OnCollectionSucessfullyReviewed;
+ [SerializeField] private UnityEvent OnCollectionUnsucessfullyReviewed;
 
     public void ReviewCollection(CollectionMB reviewedCollection)
     {
         foreach (var collectableItem in requiredItems)
-            if (!reviewedCollection.CollectedItems.Contains(collectableItem))
+            if (!reviewedCollection.CollectedItems.Contains(collectableItem)){
+ OnCollectionUnsucessfullyReviewed.Invoke();
                 return;
+            }
         OnCollectionSucessfullyReviewed.Invoke();
     }
 }
